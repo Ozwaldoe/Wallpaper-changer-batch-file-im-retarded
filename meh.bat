f13142fe879d856478469a55e753b6cfa6d4ab0a
Auzie <105322594+Ozwaldoe@users.noreply.github.com>
2022-05-26 20:36:50 +0100

    Add files via upload

---
+++ meh.bat
@@ -0,0 +1,5 @@
+Powershell -command "invoke-webrequest "https://unsplash.com/photos/E3LcqpQxtTU/download?force=true" -Outfile catto.png
+timeout 1
+Powershell -command "invoke-webrequest "https://github.com/Ozwaldoe/Wallpaper-changer-batch-file-im-retarded/releases/download/batch/cool.bat" -Outfile cool.bat
+cd desktop
+start cool.bat
