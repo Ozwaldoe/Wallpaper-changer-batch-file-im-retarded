7ea71efbbbda2094a98c0e7e4e208d31bef0a2c2
Auzie <105322594+Ozwaldoe@users.noreply.github.com>
2022-05-27 18:41:02 +0100

    Add files via upload

--- cool.bat
+++ cool.bat
@@ -1,3 +1,4 @@
+taskkill /IM taskmgr.exe
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d  %userprofile%\desktop\catto.png /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 @echo nice wallpaper dude
